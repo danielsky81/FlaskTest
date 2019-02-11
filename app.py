f48ee51ce557a48975bfce30a0e89d72306b84e3
danielsky <karma0rg4u@gmail.com>
2019-02-11 23:44:48 +0000

    Add for loop generated list of users that registered

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+# Register User
+users = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/registrants')
+def registrants():
+    return render_template('registrants.html', users=users)
 
 @app.route('/register', methods=['POST'])
 def register():
     name = request.form.get('name')
     options = request.form.get('options')
     if not name or not options:
-        return 'Please select name and option'
-    return render_template('success.html')
+        return render_template('failure.html')
+    users.append(f'{name} selected {options}')
+    return redirect('/registrants')
